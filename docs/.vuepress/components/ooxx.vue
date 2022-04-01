95d93655be5ea18f6d98a6f5bec06e4af0190ba1
Jia-Yin <jywglady@gmail.com>
2022-04-01 11:00:37 +0800

    Update

--- docs/.vuepress/components/ooxx.vue
+++ docs/.vuepress/components/ooxx.vue
@@ -19,22 +19,42 @@ export default {
     data() {
         return {
             board: null,
-            current: 'O',
+            step: 0,
+            from: "",
         }
     },
     created() {
         this.resetBoard()
+        console.log("Board", this.board)
     },
     methods: {
         resetBoard() {
             this.board = [['','',''],['','',''],['','','']]
             this.current = 'O'
+            this.step = 0
+        },
+        move(row, col) {
+            if (this.from === '' && this.board[row][col] === '') return
+            if (this.from !== '' && this.board[row][col] !== '') {
+                this.from = row*3 + col
+                return
+            }
+            this.step += 1
+            if (this.from === '') return this.from = row*3 + col
+            let frow = Math.floor(this.from/3)
+            this.board[row][col] = this.board[frow][this.from%3]
+            this.board[frow][this.from%3] = ''
+            this.$set(this.board, row, this.board[row])
+            this.$set(this.board, frow, this.board[frow])
+            this.from = ''
         },
         put(row, col) {
+            if (this.step >= 6) return this.move(row, col)
             if (this.board[row][col] !== '') return
-            this.board[row][col] = this.current
+            this.step += 1
+            this.board[row][col] = (this.step % 2 === 1) ? 'O' : 'X'
             this.$set(this.board, row, this.board[row])
-            this.current = (this.current == 'O') ? 'X' : 'O'
+            console.log("B", this.board)
         },
     }
 }
